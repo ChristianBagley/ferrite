b32b40c6069bfaf338406e278051fcc3d794506e
Nikita Marchant <nikita.marchant@gmail.com>
2017-01-18 01:40:24 +0100

    Do not track incognito mode

--- client/tab.js
+++ client/tab.js
@@ -1,6 +1,6 @@
 console.log("Loaded " + document.URL);
 
-if(false){
+if(browser.extension.inIncognitoContext){
     console.log("Ferrite: skipping this page");
 } else {
     html = document.getElementsByTagName("html")[0].outerHTML;
